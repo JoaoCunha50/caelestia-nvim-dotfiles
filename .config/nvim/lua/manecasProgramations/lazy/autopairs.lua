75e33b71042988d8194ef32d26bc7ade0a6c7b6b
JoaoCunha50 <joaomanuelmachadodacunha@gmail.com>
2026-01-19 00:26:51 +0000

    autocommit: Mon Jan 19 12:26:51 AM WET 2026

--- .config/nvim/lua/manecasProgramations/lazy/autopairs.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true
-    -- O 'config = true' é o mesmo que fazer:
-    -- config = function()
-    --   require("nvim-autopairs").setup({})
-    -- end
-}
