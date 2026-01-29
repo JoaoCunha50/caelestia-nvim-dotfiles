81c42fa5751431375514e1feff5a139539fa007c
JoaoCunha50 <joaomanuelmachadodacunha@gmail.com>
2026-01-29 00:54:48 +0000

    autocommit: Thu Jan 29 12:54:48 AM WET 2026

--- nvim/lua/manecasProgramations/lazy/lsp.lua
+++ nvim/lua/manecasProgramations/lazy/lsp.lua
@@ -78,6 +78,27 @@ return {
                     end
                 end,
             })
+            local signs = { Error = "󰅚", Warn = "󰀪", Hint = "󰌶", Info = "" }
+            for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+            end
+
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                update_in_insert = false,
+                underline = true,
+                severity_sort = true,
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    source = "always",
+                    header = "",
+                    prefix = "",
+                },
+            })
         end
     },
     {
