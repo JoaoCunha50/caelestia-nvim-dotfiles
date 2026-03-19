8d6a3037eb17bfa5230815b207901e357b854198
JoaoCunha50 <joaomanuelmachadodacunha@gmail.com>
2026-03-19 00:30:35 +0000

    autocommit: Thu Mar 19 12:30:35 AM WET 2026

--- update.sh
+++ update.sh
@@ -5,9 +5,9 @@ cd ~/dev/personal/my_dotfiles
 cp -rf ~/.config/kitty/ .
 cp -rf ~/.config/nvim/ .
 cp -rf ~/.config/fish/ .
+cp -rf ~/.claude/skills/* skills/
 cp -f ~/.local/share/caelestia/starship.toml .
 cp -f ~/.local/bin/kitty-sessionizer scripts/
-cp -f ~/.claude/skills/* skills/
 
 rm -rf kitty/kitty-utils
 
