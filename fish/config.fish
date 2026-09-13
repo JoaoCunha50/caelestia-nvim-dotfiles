416b8480984b57bbeecc509ecf5f756649a1080d
JoaoCunha50 <joaomanuelmachadodacunha@gmail.com>
2026-09-13 03:12:02 +0100

    backup a little and update configs, thinking of a better way of storing them

--- fish/config.fish
+++ fish/config.fish
@@ -1,72 +1,80 @@
 if status is-interactive
-    # Starship with transient prompt
-    function starship_transient_prompt_func
-        starship module character
-    end
+    function starship_transient_prompt_func
+        starship module character
+    end
 
-    if test "$TERM" != "linux"
-        starship init fish | source
-        enable_transience
-    end
+    if test "$TERM" != "linux"
+        starship init fish | source
+        enable_transience
+    end
 
-    # fnm
-    fnm env --use-on-cd --shell fish | source
+    # fnm
+    fnm env --use-on-cd --shell fish | source
 
-    # Direnv + Zoxide
-    command -v direnv &> /dev/null && direnv hook fish | source
-    command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
+    # Direnv + Zoxide
+    command -v direnv &> /dev/null && direnv hook fish | source
+    command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
 
-    # Better ls
-    command -v eza &> /dev/null && alias ls='eza --icons --group-directories-first -1'
+    if command -v fzf >/dev/null 2>&1
+        fzf --fish | source
+    end
 
-    # Abbrs
-    abbr lg 'lazygit'
-    abbr gd 'git diff'
-    abbr ga 'git add .'
-    abbr gc 'git commit -am'
-    abbr gl 'git log'
-    abbr gs 'git status'
-    abbr gst 'git stash'
-    abbr gsp 'git stash pop'
-    abbr gp 'git push'
-    abbr gpl 'git pull'
-    abbr gsw 'git switch'
-    abbr gsm 'git switch main'
-    abbr gb 'git branch'
-    abbr gbd 'git branch -d'
-    abbr gco 'git checkout'
-    abbr gsh 'git show'
+    if command -v eza >/dev/null 2>&1
+        alias ls 'eza -lh --group-directories-first --icons=auto'
+        alias lsa 'ls -a'
+        alias lt 'eza --tree --level=2 --long --icons --git'
+        alias lta 'lt -a'
+    end
 
-    abbr l 'ls'
-    abbr ll 'ls -l'
-    abbr la 'ls -a'
-    abbr lla 'ls -la'
+    # Abbrs
+    abbr lg 'lazygit'
+    abbr gd 'git diff'
+    abbr ga 'git add .'
+    abbr gc 'git commit -am'
+    abbr gl 'git log'
+    abbr gs 'git status'
+    abbr gst 'git stash'
+    abbr gsp 'git stash pop'
+    abbr gp 'git push'
+    abbr gpl 'git pull'
+    abbr gsw 'git switch'
+    abbr gsm 'git switch main'
+    abbr gb 'git branch'
+    abbr gbd 'git branch -d'
+    abbr gco 'git checkout'
+    abbr gsh 'git show'
 
-    abbr edit 'nvim'
+    abbr l 'ls'
+    abbr ll 'ls -l'
+    abbr la 'ls -a'
+    abbr lla 'ls -la'
 
+    abbr edit 'nvim'
 
-    # Alias
-    alias clear "printf '\033[2J\033[3J\033[1;1H'"
-    alias celar "printf '\033[2J\033[3J\033[1;1H'"
-    alias claer "printf '\033[2J\033[3J\033[1;1H'"
-    alias pamcan pacman
+    # Alias
+    alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    alias celar "printf '\033[2J\033[3J\033[1;1H'"
+    alias claer "printf '\033[2J\033[3J\033[1;1H'"
+    alias pamcan pacman
 
-    fish_add_path $HOME/.opencode/bin
-    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/.opencode/bin
+    fish_add_path $HOME/.local/bin
 
-    function dev
-        kitten @ launch --type=tab --cwd ~/dev --tab-title "dev"
-    end
+    if set -q TERM; and test "$TERM" = "xterm-kitty"
+        function kitty
+            kitty @ launch --type=tab --cwd ~/ --tab-title "home"
+        end
 
-    function configs
-        kitten @ launch --type=tab --cwd ~/.config --tab-title "configs"
-    end
+        function dev
+            kitty @ launch --type=tab --cwd ~/dev --tab-title "dev"
+        end
 
-    function study
-        kitten @ launch --type=tab --cwd ~/studys --tab-title "study"
-    end
+        function configs
+            kitty @ launch --type=tab --cwd ~/.config --tab-title "configs"
+        end
 
-    function uni
-        kitten @ launch --type=tab --cwd ~/studys/university --tab-title "university"
-    end
+        function study
+            kitty @ launch --type=tab --cwd ~/studys --tab-title "study"
+        end
+    end
 end
